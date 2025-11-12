e62be1bc1f4129c46cf8daa1039de1f71d5d4bcc
orhernan <ohercelli@gmail.com>
2025-11-12 13:28:00 +0100

    initial commit

---
+++ .test/test_ft_printf.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_printf.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orhernan <ohercelli@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 13:10:01 by orhernan          #+#    #+#             */
+/*   Updated: 2025/11/12 13:27:22 by orhernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unity.h"
+#include "ft_printf.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+void	setUp(void) {}
+void	tearDown(void) {}
+
+int	main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST();
+	return UNITY_END();
+}
+
