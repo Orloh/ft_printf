ab1debb40e8a630518de258222d75b1147770d87
Orloh <ohercelli@protonmail.com>
2025-11-14 00:18:27 +0100

    update folder structure

---
+++ inc/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orhernan <ohercelli@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 17:05:15 by orhernan          #+#    #+#             */
+/*   Updated: 2025/11/13 17:17:58 by orhernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "libft.h"
+
+/* Custom printf function*/
+int	ft_printf(const char *format);
+
+#endif
